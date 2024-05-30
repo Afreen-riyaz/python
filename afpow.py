eec77b9c3ba5a1f31f5f289a9670f44eea050351
Afreen-riyaz <afreen.riyaz789@gmail.com>
2024-05-30 12:46:18 +0530

    power of a number

---
+++ afpow.py
@@ -0,0 +1,6 @@
+a = int(input("enter base:"))
+b = int(input("enter exponent:"))
+c = a ** b
+print("power of a number without built-in function is :", c)
+d = pow(a, b)
+print("power of a number with built-in function is :", d)
